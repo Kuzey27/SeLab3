6c257eac104468ff6488b635b28f2137d8bc9d52
Ali Ahmadi <ali.ahmadi.star27@gmail.com>
2021-08-09 00:24:48 +0430

    Add setter and getter for fields of Rectangle
    + Update tests

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -10,4 +10,20 @@ public class Rectangle {
     public int computeArea() {
         return height * width;
     }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getWidth() {
+        return width;
+    }
 }
--- test/RectangleTest.java
+++ test/RectangleTest.java
@@ -1,10 +1,54 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class RectangleTest {
+    private int height;
+    private int width;
+    private Rectangle rectangle;
+
+    @Before
+    public void setup() {
+        height = 5;
+        width = 6;
+        rectangle = new Rectangle(height, width);
+    }
+
     @Test
     public void testComputeArea() {
-        Rectangle rectangle = new Rectangle(5, 6);
-        Assert.assertEquals(30, rectangle.computeArea());
+        Assert.assertEquals(width * height, rectangle.computeArea());
+    }
+
+    @Test
+    public void testHeightGetter() {
+        Assert.assertEquals(height, rectangle.getHeight());
+    }
+
+    @Test
+    public void testWidthGetter() {
+        Assert.assertEquals(width, rectangle.getWidth());
+    }
+
+    @Test
+    public void testHeightSetter() {
+        int newHeight = 13;
+        rectangle.setHeight(newHeight);
+        Assert.assertEquals(newHeight, rectangle.getHeight());
+    }
+
+    @Test
+    public void testWidthSetter() {
+        int newWidth = 17;
+        rectangle.setWidth(newWidth);
+        Assert.assertEquals(newWidth, rectangle.getWidth());
+    }
+
+    @Test
+    public void testComputeAreaAfterModification() {
+        int newHeight = 10;
+        int newWidth = 20;
+        rectangle.setHeight(newHeight);
+        rectangle.setWidth(newWidth);
+        Assert.assertEquals(newHeight * newWidth, rectangle.computeArea());
     }
 }
